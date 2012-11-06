3b3061cf277094284fe13513ed20043528f0aaee
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2012-11-06 17:34:44 +0100

    send email

--- feedback/settings.py
+++ feedback/settings.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
 RECIPIENTS = getattr(settings, 'FEEDBACK_RECIPIENTS', settings.MANAGERS)
+FROM_EMAIL = getattr(settings, 'FEEDBACK_FROM_EMAIL', settings.DEFAULT_FROM_EMAIL)
+
 NEXT_URL = getattr(settings, 'FEEDBACK_NEXT_URL', '')
 
